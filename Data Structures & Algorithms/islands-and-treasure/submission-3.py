733f2de38f59ee5308f42c643d929d2258ad493f
Dev Desai <devdesaiyt@gmail.com>
2026-09-06 19:42:14 -0400

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def islandsAndTreasure(self, grid):
+        m, n = len(grid), len(grid[0])
+        visited = {}
+        q = []
+        direc = [(1,0),(0,1),(-1,0),(0,-1)]
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    q.append((i, j))
+                    visited[(i, j)] = True
+
+        level = 0
+        while q:
+            for _ in range(len(q)):
+                r, c = q.pop(0)
+                grid[r][c] = level
+                for dr, dc in direc:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in visited and grid[nr][nc] != -1:
+                        visited[(nr, nc)] = True
+                        q.append((nr, nc))
+            level += 1
